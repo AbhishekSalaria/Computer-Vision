4e4786ec91b8bf14b3803594237a4db8dae06d33
AbhishekSalaria <36911438+AbhishekSalaria@users.noreply.github.com>
2018-08-15 18:10:10 +0530

    Update face_detect_opencv.py

--- real_time_face_detection_using_opencv/face_detect_opencv.py
+++ real_time_face_detection_using_opencv/face_detect_opencv.py
@@ -4,7 +4,7 @@ vidcap = cv2.VideoCapture(0)
 success,image = vidcap.read()
 count = 0
 success = True
-while success and count<200:
+while success and count<20:
   cv2.imwrite("training-data/s1/%d.jpg" % count, image)     # save frame as JPEG file
   success,image = vidcap.read()
   print('Read a new frame: ',success)
